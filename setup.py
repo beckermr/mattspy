9e9952c134c72b954f26a671f6218ff19b90866d
Matthew R. Becker <beckermr@users.noreply.github.com>
2015-12-09 20:56:31 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
-from setuptools import setup, find_packages
+
+setup(name="ngmixer", 
+      version="0.1.0",
+      description="Run ngmix on data",
+      license = "GPL",
+      author="Matthew R. Becker, Erin Scott Sheldon",
+      author_email="becker.mr@gmail.com, erin.sheldon@gmail.com",
+      scripts=scripts,
+      packages=['ngmixer','ngmixer.imageio','ngmixer.megamixer'])
+
+from distutils.core import setup
 import os
 
 setup (name = 'mattspy',
@@ -6,5 +16,6 @@ setup (name = 'mattspy',
        description = "Matt's python utils.",
        author = 'Matthew R. Becker',
        author_email = 'becker.mr@gmail.com',
+       license = "BSD 3-clause",
        url = 'https://github.com/beckermr/mattspy',
-       packages = find_packages())
+       packages = ['mattspy','mattspy.stats','mattspy.plotting'])
