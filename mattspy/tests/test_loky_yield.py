ed87619f99fe1757361cfe60de72fc908eedd3b6
Matthew R. Becker <becker.mr@gmail.com>
2022-02-16 11:13:29 -0600

    TST add test for loky

---
+++ mattspy/tests/test_loky_yield.py
@@ -0,0 +1,27 @@
+import time
+import joblib
+
+from ..loky_yield import LokyParallel
+
+
+def test_loky_yield():
+
+    def fun(i):
+        time.sleep(10)
+        return i
+
+    n_jobs = 100
+
+    tot = 0
+    with LokyParallel(n_jobs=2) as exc:
+        tot = 0
+        for pr in exc([joblib.delayed(fun)(i) for i in range(n_jobs)]):
+            try:
+                res = pr.result()
+            except Exception as e:
+                print(f"failure: {repr(e)}", flush=True)
+                raise e
+            else:
+                tot += res
+
+    assert tot == sum(range(n_jobs))
