fccbb4548cf3f58daa26e61a4726f811700f71fd
beckermr <becker.mr@gmail.com>
2025-07-25 13:42:02 -0500

    feat: add skeleton sklearn obkject

--- mattspy/fm/_jax_impl.py
+++ mattspy/fm/_jax_impl.py
@@ -3,59 +3,83 @@ from functools import partial
 import jax
 from jax import numpy as jnp
 
+from sklearn.base import ClassifierMixin, BaseEstimator
 
+
+@jax.jit
 def _lowrank_twoway_term(x, vmat):
-    fterm = jnp.dot(x, vmat)
-    sterm = jnp.dot(x**2, vmat**2)
-    return 0.5 * jnp.sum(fterm**2 - sterm, axis=-1)
+    fterm = jnp.einsum("np,pk...->nk...", x, vmat)
+    sterm = jnp.einsum("np,pk...->nk...", x**2, vmat**2)
+    return 0.5 * jnp.sum(fterm**2 - sterm, axis=1)
 
 
+@jax.jit
 def _fm_eval(x, w0, w, vmat):
-    return w0 + jnp.dot(x, w) + _lowrank_twoway_term(x, vmat)
+    return w0 + jnp.einsum("np,p...->n...", x, w) + _lowrank_twoway_term(x, vmat)
 
 
-@partial(jax.jit, static_argnames=("n_features", "rank"))
-def _extract_fm_params(params, n_features, rank):
-    w0 = params[0]
-    w = params[1 : 1 + n_features]
-    vmat = params[1 + n_features :].reshape((n_features, rank))
+@partial(jax.jit, static_argnames=("n_features", "rank", "n_classes"))
+def _extract_fm_params(params, n_features, rank, n_classes):
+    if n_classes is None or n_classes < 1:
+        w0 = params[0]
+        w = params[1 : 1 + n_features]
+        vmat = params[1 + n_features :].reshape((n_features, rank))
+    else:
+        w0 = params[:n_classes]
+        w = params[n_classes : n_classes + n_features * n_classes].reshape(
+            (n_features, n_classes)
+        )
+        vmat = params[n_classes + n_features * n_classes :].reshape(
+            (n_features, rank, n_classes)
+        )
+
     return w0, w, vmat
 
 
+@jax.jit
 def _combine_fm_params(w0, w, vmat):
-    return jnp.concatenate([jnp.atleast_1d(w0), w, vmat.flatten()])
+    return jnp.concatenate([jnp.atleast_1d(w0).flatten(), w.flatten(), vmat.flatten()])
 
 
-@partial(jax.jit, static_argnames=("n_classes",))
-def _fm_with_class_feature_eval(x, w0, w, vmat, class_ind, n_classes):
-    class_feature = jax.nn.one_hot(class_ind, n_classes)
-    x_with_class_feature = jnp.concatenate([class_feature, x], axis=-1)
-    return _fm_eval(x_with_class_feature, w0, w, vmat)
+class FMClassifierJAX(ClassifierMixin, BaseEstimator):
+    """A JAX-based Factorization Machine classifier.
 
+    Parameters
+    ----------
+    rank : int, optional
+        The dimension of the low-rank approximation to the
+        two-way interaction terms.
+    random_state : int, numpy RNG instance, or None
+        The RNG to use for parameter initialization.
+    batch_size : int, optional
+        The number of examples to use when fitting the estimator
+        and making predictions. The value None indicates to use all
+        examples.
+    lambda_v : float, optional
+        The L2 regularization strength to use for the low-rank embedding
+        matrix.
+    lambda_w : float, optional
+        The L2 regularization strength to use for the linear terms.
+    init_scale : float, optional
+        The RMS of the Gaussian parameter initialization.
+    solver : optimistix.AbstractMinimiser or str, optional
+        The solver the use with the `optimistix` package. If a string is passed, the
+        corresponding attribute from the `optax` package is used via
+        `optimistix.OptaxMinimiser`.
+    solver_kwargs : dict, optional
+        An optional dictionary of keyword arguments to pass to the solver. These
+        keywords are passed directly, except for `max_steps` which is passed to
+        `optimistix.minimise` directly. The `max_steps` keyword is ignored during
+        partial fitting.
 
-_vmap_fm_with_class_feature_eval = jax.jit(
-    jax.vmap(
-        _fm_with_class_feature_eval,
-        in_axes=(None, None, None, None, -1, None),
-        out_axes=-1,
-    ),
-    static_argnames=("n_classes",),
-)
+    Attributes
+    ----------
+    n_dims_ : int
+        Number of features in X.
+    classes_ : array
+        Class labels from the data.
+    n_classes_ : int
+        Number of unique class labels from the data.
+    """
 
-
-@partial(jax.jit, static_argnames=("n_classes",))
-def _logits_fm_eval(x, w0, w, vmat, n_classes):
-    class_inds = jnp.arange(n_classes, dtype=int)
-    if jnp.ndim(x) == 2:
-        class_inds = jnp.tile(class_inds, (x.shape[0], 1))
-    return _vmap_fm_with_class_feature_eval(x, w0, w, vmat, class_inds, n_classes)
-
-
-@partial(jax.jit, static_argnames=("n_classes",))
-def _log_softmax_fm_eval(x, w0, w, vmat, n_classes):
-    return jax.nn.log_softmax(_logits_fm_eval(x, w0, w, vmat, n_classes), axis=-1)
-
-
-@partial(jax.jit, static_argnames=("n_classes",))
-def _softmax_fm_eval(x, w0, w, vmat, n_classes):
-    return jax.nn.softmax(_logits_fm_eval(x, w0, w, vmat, n_classes), axis=-1)
+    pass
--- mattspy/fm/tests/test_jax_impl.py
+++ mattspy/fm/tests/test_jax_impl.py
@@ -1,4 +1,3 @@
-import scipy.special
 import numpy as np
 
 import pytest
@@ -8,21 +7,23 @@ from mattspy.fm._jax_impl import (
     _fm_eval,
     _extract_fm_params,
     _combine_fm_params,
-    _fm_with_class_feature_eval,
-    _log_softmax_fm_eval,
-    _softmax_fm_eval,
 )
 
 
-def _gen_test_data(n_samples, n_features, rank, flatten=False, seed=42):
+def _gen_test_data(n_samples, n_features, rank, flatten=False, seed=42, n_classes=None):
     rng = np.random.default_rng(seed=seed)
     x = rng.normal(size=(n_samples, n_features))
     if flatten:
         x = x.flatten()
 
-    vmat = rng.normal(size=(n_features, rank))
-    w = rng.normal(size=(n_features))
-    w0 = rng.normal()
+    if n_classes is None:
+        vmat = rng.normal(size=(n_features, rank))
+        w = rng.normal(size=(n_features))
+        w0 = rng.normal()
+    else:
+        vmat = rng.normal(size=(n_features, rank, n_classes))
+        w = rng.normal(size=(n_features, n_classes))
+        w0 = rng.normal(size=n_classes)
 
     return {"x": x, "vmat": vmat, "w": w, "w0": w0}
 
@@ -48,23 +49,27 @@ def test_fm_lowrank_twoway_term(n_samples):
     np.testing.assert_allclose(vals, true_vals, rtol=1e-5, atol=1e-5)
 
 
-def test_fm_lowrank_twoway_term_flat():
-    n_samples = 1
+@pytest.mark.parametrize("n_samples", (1, 10))
+@pytest.mark.parametrize("n_classes", (1, 7))
+def test_fm_lowrank_twoway_term_classes(n_samples, n_classes):
     n_features = 13
     rank = 4
-    data = _gen_test_data(n_samples, n_features, rank, flatten=True)
+    data = _gen_test_data(
+        n_samples, n_features, rank, flatten=False, n_classes=n_classes
+    )
     x = data["x"]
     vmat = data["vmat"]
 
     vals = _lowrank_twoway_term(x, vmat)
     true_vals = []
-    _val = 0.0
-    for i in range(x.shape[0]):
-        for j in range(i + 1, x.shape[0]):
-            _val += np.sum(vmat[i, :] * vmat[j, :]) * x[i] * x[j]
-    true_vals.append(_val)
+    for k in range(x.shape[0]):
+        _val = 0.0
+        for i in range(x.shape[1]):
+            for j in range(i + 1, x.shape[1]):
+                _val += np.sum(vmat[i, ...] * vmat[j, ...], axis=0) * x[k, i] * x[k, j]
+        true_vals.append(_val)
 
-    assert vals.shape == ()
+    assert vals.shape == (x.shape[0], n_classes)
     np.testing.assert_allclose(vals, true_vals, rtol=1e-5, atol=1e-5)
 
 
@@ -92,11 +97,14 @@ def test_fm_eval(n_samples):
     np.testing.assert_allclose(vals, true_vals, rtol=1e-5, atol=1e-5)
 
 
-def test_fm_eval_flat():
-    n_samples = 1
+@pytest.mark.parametrize("n_samples", (1, 10))
+@pytest.mark.parametrize("n_classes", (1, 7))
+def test_fm_eval_classes(n_samples, n_classes):
     n_features = 13
     rank = 4
-    data = _gen_test_data(n_samples, n_features, rank, flatten=True)
+    data = _gen_test_data(
+        n_samples, n_features, rank, flatten=False, n_classes=n_classes
+    )
     x = data["x"]
     vmat = data["vmat"]
     w = data["w"]
@@ -104,176 +112,47 @@ def test_fm_eval_flat():
 
     vals = _fm_eval(x, w0, w, vmat)
     true_vals = []
-    _val = w0
-    _val += np.sum(x * w)
-    for i in range(x.shape[0]):
-        for j in range(i + 1, x.shape[0]):
-            _val += np.sum(vmat[i, :] * vmat[j, :]) * x[i] * x[j]
-    true_vals.append(_val)
+    for k in range(x.shape[0]):
+        _val = w0.copy()
+        _val += np.sum(x[k, :].reshape(-1, 1) * w, axis=0)
+        for i in range(x.shape[1]):
+            for j in range(i + 1, x.shape[1]):
+                _val += np.sum(vmat[i, ...] * vmat[j, ...], axis=0) * x[k, i] * x[k, j]
+        true_vals.append(_val)
 
-    assert vals.shape == ()
+    assert vals.shape == (x.shape[0], n_classes)
     np.testing.assert_allclose(vals, true_vals, rtol=1e-5, atol=1e-5)
 
 
+@pytest.mark.parametrize("n_classes", [-1, 0, None, 1, 7])
 @pytest.mark.parametrize("n_features", [1, 3, 11])
 @pytest.mark.parametrize("rank", [1, 3])
-def test_fm_extract_combine_fm_params(n_features, rank):
-    data = _gen_test_data(1, n_features, rank)
+def test_fm_extract_combine_fm_params(n_features, rank, n_classes):
+    data = _gen_test_data(
+        1,
+        n_features,
+        rank,
+        n_classes=None if n_classes is None or n_classes < 1 else n_classes,
+    )
     w0 = data["w0"]
     w = data["w"]
     vmat = data["vmat"]
 
     params = _combine_fm_params(w0, w, vmat)
-    np.testing.assert_allclose(params[0], w0)
-    np.testing.assert_allclose(params[1 : 1 + n_features], w)
-    np.testing.assert_allclose(params[1 + n_features :], vmat.flatten())
+    if n_classes is None or n_classes < 1:
+        np.testing.assert_allclose(params[0], w0)
+        np.testing.assert_allclose(params[1 : 1 + n_features], w)
+        np.testing.assert_allclose(params[1 + n_features :], vmat.flatten())
+    else:
+        np.testing.assert_allclose(params[:n_classes], w0)
+        np.testing.assert_allclose(
+            params[n_classes : n_classes + n_features * n_classes], w.flatten()
+        )
+        np.testing.assert_allclose(
+            params[n_classes + n_features * n_classes :], vmat.flatten()
+        )
 
-    tw0, tw, tvmat = _extract_fm_params(params, n_features, rank)
+    tw0, tw, tvmat = _extract_fm_params(params, n_features, rank, n_classes)
     np.testing.assert_allclose(tw0, w0)
     np.testing.assert_allclose(tw, w)
     np.testing.assert_allclose(tvmat, vmat)
-
-
-@pytest.mark.parametrize("n_samples", [1, 10])
-@pytest.mark.parametrize("n_classes", [2, 13])
-@pytest.mark.parametrize("class_ind", [0, 1])
-def test_fm_with_class_feature_eval(class_ind, n_classes, n_samples):
-    data = _gen_test_data(n_samples, 17, 5)
-    x = data["x"][:, :-n_classes]
-    w0 = data["w0"]
-    w = data["w"]
-    vmat = data["vmat"]
-    _class_ind = np.zeros_like(x[:, 0], dtype=int) + class_ind
-    if x.shape[0] > 1:
-        _class_ind[5:] = 1 - class_ind
-    vals = _fm_with_class_feature_eval(
-        x,
-        w0,
-        w,
-        vmat,
-        _class_ind,
-        n_classes,
-    )
-    assert vals.shape == x.shape[:1]
-
-    class_feat = np.zeros((x.shape[0], n_classes))
-    class_feat[:, class_ind] = 1.0
-    if x.shape[0] > 1:
-        class_feat[5:, class_ind] = 0.0
-        class_feat[5:, 1 - class_ind] = 1.0
-    x = np.concatenate([class_feat, x], axis=1)
-    true_vals = []
-    for k in range(x.shape[0]):
-        _val = w0
-        _val += np.sum(x[k, :] * w)
-        for i in range(x.shape[1]):
-            for j in range(i + 1, x.shape[1]):
-                _val += np.sum(vmat[i, :] * vmat[j, :]) * x[k, i] * x[k, j]
-        true_vals.append(_val)
-
-    np.testing.assert_allclose(vals, true_vals, rtol=1e-5, atol=1e-5)
-
-
-@pytest.mark.parametrize("n_classes", [2, 13])
-@pytest.mark.parametrize("class_ind", [0, 1])
-def test_fm_with_class_feature_eval_flat(class_ind, n_classes):
-    data = _gen_test_data(1, 17, 5, flatten=True)
-    x = data["x"][:-n_classes]
-    w0 = data["w0"]
-    w = data["w"]
-    vmat = data["vmat"]
-    vals = _fm_with_class_feature_eval(
-        x,
-        w0,
-        w,
-        vmat,
-        class_ind,
-        n_classes,
-    )
-    assert vals.shape == ()
-
-    class_feat = np.zeros(n_classes)
-    class_feat[class_ind] = 1.0
-    x = np.concatenate([class_feat, x], axis=0)
-    true_vals = []
-    _val = w0
-    _val += np.sum(x[:] * w)
-    for i in range(x.shape[0]):
-        for j in range(i + 1, x.shape[0]):
-            _val += np.sum(vmat[i, :] * vmat[j, :]) * x[i] * x[j]
-    true_vals.append(_val)
-
-    np.testing.assert_allclose(vals, true_vals, rtol=1e-5, atol=1e-5)
-
-
-@pytest.mark.parametrize("n_samples", [1, 10])
-@pytest.mark.parametrize("n_classes", [2, 13])
-def test_fm_log_softmax_fm_eval(n_classes, n_samples):
-    data = _gen_test_data(n_samples, 17, 5)
-    x = data["x"][:, :-n_classes]
-    w0 = data["w0"]
-    w = data["w"]
-    vmat = data["vmat"]
-    vals = _log_softmax_fm_eval(x, w0, w, vmat, n_classes)
-    assert vals.shape == (n_samples, n_classes)
-    np.testing.assert_allclose(np.sum(np.exp(vals), axis=-1), 1.0, rtol=1e-5, atol=1e-5)
-
-    exp_vals = _softmax_fm_eval(x, w0, w, vmat, n_classes)
-    assert exp_vals.shape == (n_samples, n_classes)
-    np.testing.assert_allclose(exp_vals, np.exp(vals), rtol=1e-5, atol=1e-5)
-
-    true_vals = []
-    for k in range(x.shape[0]):
-        _true_vals = []
-        for cind in range(n_classes):
-            class_feat = np.zeros((x.shape[0], n_classes))
-            class_feat[:, cind] = 1.0
-            _x = np.concatenate([class_feat, x], axis=1)
-
-            _val = w0
-            _val += np.sum(_x[k, :] * w)
-            for i in range(_x.shape[1]):
-                for j in range(i + 1, _x.shape[1]):
-                    _val += np.sum(vmat[i, :] * vmat[j, :]) * _x[k, i] * _x[k, j]
-
-            _true_vals.append(_val)
-        true_vals.append(_true_vals)
-
-    true_vals = np.array(true_vals)
-    true_vals -= scipy.special.logsumexp(true_vals, axis=1, keepdims=True)
-
-    np.testing.assert_allclose(vals, true_vals, rtol=1e-5, atol=1e-5)
-
-
-@pytest.mark.parametrize("n_classes", [2, 13])
-def test_fm_log_softmax_fm_eval_flat(n_classes):
-    data = _gen_test_data(1, 17, 5, flatten=True)
-    x = data["x"][:-n_classes]
-    w0 = data["w0"]
-    w = data["w"]
-    vmat = data["vmat"]
-    vals = _log_softmax_fm_eval(x, w0, w, vmat, n_classes)
-    assert vals.shape == (n_classes,)
-    np.testing.assert_allclose(np.sum(np.exp(vals), axis=-1), 1.0, rtol=1e-5, atol=1e-5)
-
-    exp_vals = _softmax_fm_eval(x, w0, w, vmat, n_classes)
-    assert exp_vals.shape == (n_classes,)
-    np.testing.assert_allclose(exp_vals, np.exp(vals), rtol=1e-5, atol=1e-5)
-
-    true_vals = []
-    for cind in range(n_classes):
-        class_feat = np.zeros(n_classes)
-        class_feat[cind] = 1.0
-        _x = np.concatenate([class_feat, x], axis=-1)
-
-        _val = w0
-        _val += np.sum(_x * w)
-        for i in range(_x.shape[0]):
-            for j in range(i + 1, _x.shape[0]):
-                _val += np.sum(vmat[i, :] * vmat[j, :]) * _x[i] * _x[j]
-
-        true_vals.append(_val)
-    true_vals = np.array(true_vals)
-    true_vals -= scipy.special.logsumexp(true_vals, axis=-1, keepdims=True)
-
-    np.testing.assert_allclose(vals, true_vals, rtol=1e-5, atol=1e-5)
